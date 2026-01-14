096ad40d896c1c2a0b250d2a0049ee687b5198d5
Ek4m <elvin.james.99@gmail.com>
2026-01-14 19:15:49 +0400

    Register password schema fixing

--- src/modules/auth/schemas.js
+++ src/modules/auth/schemas.js
@@ -17,7 +17,7 @@ const registerSchema = z.object({
     .regex(/[a-z]/, "Password must contain at least one lowercase letter")
     .regex(/[0-9]/, "Password must contain at least one number")
     .regex(
-      /[^a-zA-Z0-9]/,
+      /^(?=.*[^a-zA-Z0-9])/,
       "Password must contain at least one special character"
     ),
 });
